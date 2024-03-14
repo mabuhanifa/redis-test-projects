00007deaa1df1b85eb6622c4dcf41e53905bdf87
mabuhanifa <moh.abuhanifa@gmail.com>
2024-03-14 10:07:33 +0600

    redis packages changed

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+require("dotenv").config();
+app.use(cors());
+app.use(express.json());
+
 const port = process.env.PORT || 3000;
 // const redis = require("redis");
 // const createClient = redis.createClient;
 const redis = require("ioredis");
 
 const client = new redis(process.env.REDIS_URL);
-
-require("dotenv").config();
 
 async function check() {
   // await client.connect();
@@ -17,11 +19,9 @@ async function check() {
 
   const value = await client.get("dsadad");
 
-  console.log(value);
+  console.log({ value });
 }
 check();
-app.use(cors());
-app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
